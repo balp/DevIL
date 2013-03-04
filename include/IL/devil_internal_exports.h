7937525afff3dad58ffc230ef2a201fbf2f36d6b
Anders Arnholm <Anders.Arnholm@mecel.se>
2013-03-04 10:33:42 +0100

    Set NOINLINE better on Windows.
    Change-Id: I2cc215c212d4fb48d71971169d58f264fc932d90
    Reviewed-on: http://10.236.89.5:8080/2709
    Scrum-Card: Anders Arnholm <Anders.Arnholm@mecel.se>
    Reviewed-by: Anders Arnholm <Anders.Arnholm@mecel.se>
    Tested-by: Anders Arnholm <Anders.Arnholm@mecel.se>

--- include/IL/devil_internal_exports.h
+++ include/IL/devil_internal_exports.h
@@ -27,12 +27,15 @@
 	#define INLINE extern inline
 #elif defined(_MSC_VER)	//@TODO: Get this working in MSVC++.
 						//  http://www.greenend.org.uk/rjk/2003/03/inline.html
-	#define NOINLINE
-	//#define INLINE
+	#ifndef NOINLINE	
+		#define INLINE __inline
+	#else
+		#define INLINE
+	#endif
 	/*#ifndef _WIN64  // Cannot use inline assembly in x64 target platform.
 		#define USE_WIN32_ASM
 	#endif//_WIN64*/
-	#define INLINE __inline
+	
 #else
 	#define INLINE inline
 #endif
